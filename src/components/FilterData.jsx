99c96e41a8966c34c1ef37fa567d3b82ff858c99
Lalith Chakradhar <lalith.rangana.31.3@mountblue.tech>
2025-02-21 12:13:07 +0530

    Completed the homepage of the rest-countries project

--- src/components/FilterData.jsx
+++ src/components/FilterData.jsx
@@ -4,7 +4,7 @@ import FilterRegion from './FilterRegion';
 
 const FilterData = () => {
   return (
-    <div className='flex flex-col mx-3 lg:flex-row lg:justify-between lg:items-center lg:mx-16 lg:my-8'>
+    <div className='flex flex-col mx-3 my-6 lg:flex-row lg:justify-between lg:items-center lg:mx-16 lg:my-8'>
         <CountrySearch/>
         <FilterRegion/>
     </div>
